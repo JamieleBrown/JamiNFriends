76125bd55aad665f33acfa436d32398d4e52c97b
AlphaBetaJamiele <thetruejamiele@gmail.com>
2022-10-20 20:40:50 -0400

    Revert "Literally Everything"
    This reverts commit 33ff90314d6c8b4fbe69fd555da267cd404fca41.

--- example_mods/data/Interaction/Hey.lua
+++
@@ -1,5 +0,0 @@
-function onUpdate(elapsed)
-if keyPressed('space') then
-characterPlayAnim('jami_demo', 'hey', true);
-end
-end
--- example_mods/data/Overheated/Be Careful.lua
+++
@@ -1,6 +0,0 @@
-function opponentNoteHit(elapsed)
-    health = getProperty('health')
-    if getProperty('health') > 0.25 then
-        setProperty('health', health- 0.025);
-    end
-end
